db8edf832e5a5d9708cbd42dba5e6b97f96ddb0a
Ridwan <26ridwanfirdaus@gmail.com>
2022-08-16 11:18:29 +0700

    Initiate

---
+++ www.designesia.com/themes/lovus/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date('09/04/2022 11:00 AM')}); // year, month, date, hour
+        });
+});		
+
